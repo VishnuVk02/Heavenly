a8146b69f93c6b284e1aa9b47062e46c47af14a8
VishnuVk02 <vk7558689@gmail.com>
2025-03-17 22:17:35 +0530

    updated pictures,resort names ,location

--- app/src/main/java/com/example/heavenly/bangalore_page.java
+++ app/src/main/java/com/example/heavenly/bangalore_page.java
@@ -1,15 +1,30 @@
 package com.example.heavenly;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class bangalore_page extends AppCompatActivity {
+    TextView tv1,tv2,tv3,tv4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bangalore_page);
+
+        tv1 = findViewById(R.id.txtr1);
+        tv1.setPaintFlags(tv1.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+        tv2 = findViewById(R.id.txtr2);
+        tv2.setPaintFlags(tv2.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+        tv3 = findViewById(R.id.txtr3);
+        tv3.setPaintFlags(tv3.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+        tv4 = findViewById(R.id.txtr4);
+        tv4.setPaintFlags(tv4.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
 
     }
 }
--- app/src/main/java/com/example/heavenly/hyderabad_page.java
+++ app/src/main/java/com/example/heavenly/hyderabad_page.java
@@ -1,16 +1,31 @@
 package com.example.heavenly;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class hyderabad_page extends AppCompatActivity {
 //    Button back;
 
+    TextView tv1,tv2,tv3,tv4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hyderabad_page);
+
+        tv1 = findViewById(R.id.txtr1);
+        tv1.setPaintFlags(tv1.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+        tv2 = findViewById(R.id.txtr2);
+        tv2.setPaintFlags(tv2.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+        tv3 = findViewById(R.id.txtr3);
+        tv3.setPaintFlags(tv3.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+        tv4 = findViewById(R.id.txtr4);
+        tv4.setPaintFlags(tv4.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
 
     }
 }
